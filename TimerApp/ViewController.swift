a82d22082ab7749e7056cd93f2c82848486db513
Dyllanruth <Dyllan.ruth@gmail.com>
2022-04-08 07:55:35 -0500

    first push

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TodaysDate: UILabel!
+    
+    @IBOutlet weak var CurrentTime: UILabel!
+    
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        //Step 1 Read the system date and time information
+        let currentDate = NSDate()
+        //Step 2 display date on the dateLabel
+        let myDateFormattor = DateFormatter()
+        myDateFormattor.dateFormat = "MMMM dd, yyyy"
+        let convertDateString = myDateFormattor.string(from: currentDate as Date)
+        
+        TodaysDate.text =  convertDateString
+        //Step 3 display time on the timeLabel
+        myDateFormattor.dateFormat = "hh:mm:ss"
+        let convertTimeString = myDateFormattor.string(from: currentDate as Date)
+        
+        CurrentTime.text = convertTimeString
+        
+        
     }
 
 
